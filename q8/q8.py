8c9786c67886d4fbdc663fddca1710d0d48a7b16
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-06-02 00:16:41 +0800

    Create q8.py

---
+++ q8/q8.py
@@ -0,0 +1,68 @@
+import time
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pandas as pd
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.agoda.com/zh-tw/")
+wait = WebDriverWait(driver, 10)
+
+search_city = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="textInput"]')))
+search_city.send_keys('台中')
+time.sleep(1)
+search_city.send_keys(Keys.ESCAPE)
+
+search_button = wait.until(EC.presence_of_element_located(
+    (By.XPATH, '//button[@class="Buttonstyled__ButtonStyled-sc-5gjk6l-0 iCZpGI Box-sc-kv6pi1-0 fDMIuA"]')))
+search_button.click()
+driver.switch_to.window(driver.window_handles[-1])
+
+hotel_item = wait.until(EC.presence_of_all_elements_located(
+    (By.CSS_SELECTOR, '[data-selenium="hotel-item"]')))
+count_down = 1
+while True:
+    try:
+        actions = ActionChains(driver)
+        actions.move_to_element(hotel_item[count_down]).perform()
+        count_down += 1
+        time.sleep(0.1)
+        hotel_item = wait.until(EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, '[data-selenium="hotel-item"]')))
+        driver.execute_script("window.scrollBy(0, 1000);")
+    except IndexError as out_range:
+        print('WE ARRIVED')
+        break
+    except NameError as unknown_exception_unsolved_name:
+        print('might end,or just bugged')
+        print(unknown_exception_unsolved_name)
+        break
+
+hotel_name_list, hotel_price_list = [], []
+
+for hotel in hotel_item:
+    hotel_name = hotel.find_element(By.XPATH, './/h3[@data-selenium="hotel-name"]').text
+    try:
+        hotel_price = hotel.find_element(By.XPATH,
+                                         './/div[@data-element-name="final-price"]/span[@data-selenium="display-price"]').text
+    except NoSuchElementException as slow:
+        try:
+            hotel_price = hotel.find_element(By.XPATH, './/div[@data-element-name="final-price"]').text
+            hotel_price = '慢了一步 ' + hotel_price
+        except Exception as ex:
+            hotel_price = '甚麼都沒有'
+    print(hotel_name, '   ', hotel_price)
+    hotel_name_list.append(hotel_name)
+    hotel_price_list.append(hotel_price)
+
+data = zip(hotel_name_list, hotel_price_list)
+df = pd.DataFrame(data, columns=['Name', 'Price'])
+df.to_csv('agoda_result.csv', index=False, encoding='utf-8-sig')
