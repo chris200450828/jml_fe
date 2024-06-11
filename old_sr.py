a78fba84e5f2a23a520adc649f1187f989aebc3a
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-06-11 22:31:19 +0800

    Create old_sr.py

---
+++ old_sr.py
@@ -0,0 +1,36 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def scroll_to_lowest(driver, method, locate, start_count=0, mouse_wait_time=0.1, return_number=None):
+    wait = WebDriverWait(driver, 10)
+    count_down = start_count
+    if method == 'CSS':
+        method = By.CSS_SELECTOR
+    elif method == 'XPATH':
+        method = By.XPATH
+    elif method == By.XPATH or method == By.CSS_SELECTOR:
+        pass
+    else:
+        raise ValueError('Method must be XPATH,CSS or selenium.By object')
+
+    while True:
+        try:
+            elements = wait.until(EC.presence_of_all_elements_located((method, locate)))
+            actions = ActionChains(driver)
+            actions.move_to_element(elements[count_down]).perform()
+            count_down += 1
+            time.sleep(mouse_wait_time)
+            driver.execute_script("window.scrollBy(0, 1000);")
+        except IndexError:
+            break
+        except NameError as unknown_exception_unsolved_name:
+            print('might end, or just bugged')
+            print(unknown_exception_unsolved_name)
+            break
+
+    if return_number is True:
+        return count_down
