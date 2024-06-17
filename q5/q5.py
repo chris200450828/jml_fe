b2696082643ba2588ca8f2b67a0c6ee02bba8243
cleverfishfishfish <162530106+cleverfishfishfish@users.noreply.github.com>
2024-06-17 21:56:26 +0800

    Update q5.py

--- q5/q5.py
+++ q5/q5.py
@@ -1,27 +1,25 @@
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options 
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-chrome_options = Options()
+# 設定 Chrome 瀏覽器選項
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_experimental_option("detach", True)
 
-# 創建 Chrome WebDriver 實例
-chrome_driver_path = "C:/Users/user/Desktop/網路程式設計/chromedriver-win64/chromedriver.exe"
-service = ChromeService(executable_path=chrome_driver_path)
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-driver.implicitly_wait(10)
-
-# 前往指定網址
-url = "https://www.momoshop.com.tw/main/Main.jsp?cid=memb&oid=back2hp&mdiv=1099800000-bt_0_150_01-bt_0_150_01_e1&ctype=B"
-driver.get(url)
+# 啟動 Chrome 瀏覽器
+driver = webdriver.Chrome(options=options)
+driver.get(
+    "https://www.momoshop.com.tw/main/Main.jsp?cid=memb&oid=back2hp&mdiv=1099800000-bt_0_150_01-bt_0_150_01_e1&ctype=B")
+wait = WebDriverWait(driver, 10)
 
 # 找到搜索框
 search_box = driver.find_element(By.ID, "keyword")
 
-# 在搜索框輸入 "nba"，你可以自行更改關鍵字
+# 在搜索框輸入 "nba"
 search_box.send_keys("nba")
 
 # 找到搜尋按鈕並點擊
@@ -34,9 +32,16 @@ WebDriverWait(driver, 10).until(EC.url_contains("https://www.momoshop.com.tw/sea
 # 抓取網頁內容
 html_content = driver.page_source
 
+# 使用正則表達式查找並替換關鍵字
+new_html_content = re.sub(
+    r'location\.replace\(\'https:\/\/www\.momoshop\.com\.tw\/search\/searchShop\.jsp\?keyword=[^\']*\'\);',
+    'location.replace(\'https://www.momoshop.com.tw/search/searchShop.jsp?keyword=nba\');',
+    html_content
+)
+
 # 將 HTML 內容保存到文件中
-with open("C:/Users/user/Desktop/網路程式設計/期末/NBA_test.html", "w", encoding="utf-8") as f:
-    f.write(html_content)
+with open("C:/Users/user/Desktop/大學二下作業/網路程式設計/期末/NBA_test.html", "w", encoding="utf-8") as f:
+    f.write(new_html_content)
 
 # 關閉瀏覽器
 driver.quit()
